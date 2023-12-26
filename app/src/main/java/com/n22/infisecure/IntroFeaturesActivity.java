195ac5883a51d4cc454c2eb7d7ed808588fefeb6
Nishanthan M Ravichandran <nishanthravi226@gmail.com>
2023-12-26 01:41:37 -0500

    integrated facebook login and designed till sign up page

--- app/src/main/java/com/n22/infisecure/IntroFeaturesActivity.java
+++ app/src/main/java/com/n22/infisecure/IntroFeaturesActivity.java
@@ -1,28 +1,34 @@
 package com.n22.infisecure;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
-
-import android.os.Bundle;
 
 import com.n22.infisecure.adapters.IntroFeaturesAdapter;
 import com.n22.infisecure.fragments.IFAuthFragment;
 import com.n22.infisecure.fragments.IFProfileFragment;
 import com.n22.infisecure.fragments.IFSecurityFragment;
-import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
 import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
 
 public class IntroFeaturesActivity extends AppCompatActivity {
     ViewPager viewPager;
     WormDotsIndicator dotsIndicator;
+    Button skipButton;
+    RelativeLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_features);
+        layout = findViewById(R.id.layout);
         viewPager = findViewById(R.id.viewPager);
         dotsIndicator = findViewById(R.id.worm_dot);
-
+        skipButton = findViewById(R.id.btnSkip);
         IntroFeaturesAdapter introFeaturesAdapter = new IntroFeaturesAdapter(getSupportFragmentManager());
         introFeaturesAdapter.addFragment(new IFAuthFragment(), "AuthFragment");
         introFeaturesAdapter.addFragment(new IFSecurityFragment(), "SecurityFragment");
@@ -31,6 +37,21 @@ public class IntroFeaturesActivity extends AppCompatActivity {
         viewPager.setAdapter(introFeaturesAdapter);
         dotsIndicator.attachTo(viewPager);
 
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {gotoUserExistencePage();}
+        });
 
+    }
+
+    private void gotoUserExistencePage() {
+//        final Dialog dialog = new Dialog(IntroFeaturesActivity.this);
+//       // dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        dialog.setContentView(R.layout.activity_user_existence);
+//        dialog.show();
+        Intent intent = new Intent(IntroFeaturesActivity.this, UserExistenceActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/n22/infisecure/constants/AppConstants.java
+++ app/src/main/java/com/n22/infisecure/constants/AppConstants.java
@@ -4,4 +4,9 @@ public class AppConstants {
 
     public static final String INFI_SECURE = "InfiSecure";
     public static final int SPLASH_DISPLAY_SECONDS = 3;
+    public static final String EMAIL = "email";
+    public static final String PUBLIC_PROFILE = "public_profile";
+    public static final String FACEBOOK = "FACEBOOK";
+    public static final String GOOGLE_AUTH = "GOOGLE_AUTH";
+
 }
